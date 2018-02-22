0be52179bd1df46f5f99d426ec124ae8377ce8f7
yutao <121595113@qq.com>
2018-02-22 15:17:43 +0800

    初始化项目

---
+++ demo04/src/js/main.js
@@ -0,0 +1,11 @@
+require('../css/app.css');
+require('../css/test.scss');
+
+// 图片
+let img1 = document.createElement('img');
+img1.src = require('../images/small.png');
+document.body.appendChild(img1);
+
+let img2 = document.createElement('img');
+img2.src = require('../images/big.png');
+document.body.appendChild(img2);
---
+++ demo04/webpack.config.js
@@ -0,0 +1,71 @@
+const path = require('path');
+const webpack = require('webpack');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const HtmlwebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    // common: ['common'],
+    main: './src/js/main.js',
+    main2: './src/js/main2.js',
+  },
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    publicPath: './',
+    filename: 'js/[name].js'
+  },
+  module: {
+    rules: [{
+      test: /\.js[x]?$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['es2015', 'react']
+        }
+      }
+    }, {
+      test: /\.css$/,
+      use: ['style-loader', 'css-loader']
+    }, {
+      test: /\.scss$/,
+      use: ['style-loader', 'css-loader', 'sass-loader']
+    }, {
+      test: /\.(png|jpg)$/,
+      use: [{
+        loader: 'url-loader',
+        options: {
+          limit: 8192,
+          name: 'images/[name]-[hash:8].[ext]'
+        }
+      }]
+    }]
+  },
+  plugins: [
+    new UglifyJsPlugin(),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'common',
+      filename: 'js/common.js',
+    }),
+    new HtmlwebpackPlugin({
+      title: 'Webpack-demos',
+      filename: 'index.html',
+      template: 'src/index.html',
+      hash: true,
+      // minify: { //压缩HTML文件
+      //   removeComments: true, //移除HTML中的注释
+      //   collapseWhitespace: true, //删除空白符与换行符，
+      //   minifyCSS: true, //压缩style标签和style属性中的样式
+      //   minifyJS: true //压缩script标签和事件属性中的样式
+      // },
+      chunks: ['common', 'main']
+    }),
+    new HtmlwebpackPlugin({
+      title: 'Webpack-demos',
+      filename: 'main2.html',
+      template: 'src/index.html',
+      hash: true,
+      chunks: ['common', 'main2']
+    }),
+  ]
+};
